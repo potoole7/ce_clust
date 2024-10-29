fddeda583adc00f38109ad5464d3ac79eb557761
paddy7wb <paddy7wb@gmail.com>
2024-10-29 14:52:28 +0000

    Add functions to support functions file

--- src/01_irish_ce/02_vignotto_clust.R
+++ src/01_irish_ce/02_vignotto_clust.R
@@ -29,6 +29,7 @@ library(transport)
 library(twosamples)
 
 source("src/functions.R")
+source("src/01_irish_ce/functions.R")
 
 theme <- theme_bw() +
   theme(
@@ -44,21 +45,6 @@ theme <- theme_bw() +
   )
 
 sf::sf_use_s2(FALSE)
-
-#### Functions ####
-
-# compute the total within-cluster sum of distances
-within_cluster_sum <- function(k, distance_matrix) {
-  kmedoids_result <- pam(distance_matrix, k = k)
-  return(kmedoids_result$objective[1])  # Total cost (sum of distances)
-}
-
-# Define the KL divergence function
-# kl_divergence <- function(p, q) {
-#   p <- p / sum(p)
-#   q <- q / sum(q)
-#   sum(p * log(p / q), na.rm = TRUE)
-# }
 
 #### Load Data ####
 
@@ -107,44 +93,9 @@ pts <- ab_df %>%
   st_to_sf()
 
 # calculate adjacency matrix from Voronoi cells
-calc_adj_mat <- \(pts, cut_vor = TRUE, plot = FALSE) {
-  # Calculate voronoi partition for sites
-  vor <- pts %>% 
-    st_union() %>%
-    st_voronoi(envelope = st_as_sfc(st_bbox(pts))) %>%
-    # st_voronoi(envelope = st_as_sfc(st_bbox(areas))) %>%
-    st_collection_extract(type = "POLYGON") %>% 
-    st_sf() %>%  # convert from geometry set to simple feature collection
-    identity()
-  
-  # order voronoi cells to match points
-  vor <- vor[order(st_nearest_feature(st_centroid(vor), pts)), ]
-  
-  # cutoff voronoi cells from ocean, if desired (stops far away neighbours)
-  # TODO: Cut via coastline
-  if (cut_vor == TRUE) {
-    # slightly smaller than areas bbox
-    vor <- st_crop(
-      vor, c("xmin" = -10, "ymin" = 51.6, "xmax" = -6, "ymax" = 55.2)
-    )
-  }
-   
-  # check that voronoi cells have been produced correctly
-  if (plot == TRUE) {
-    plot(st_geometry(areas)) 
-    plot(vor, add = TRUE)
-    plot(pts, col = "blue", pch = 16, add = TRUE)
-    # test that voronoi cells and points ordered correctly
-    # plot(vor[10, ], col = "blue", fill = NA, lwd = 5, add = TRUE)
-    # plot(pts[10, ], col = "red", pch = 16, add = TRUE)
-  }
-  
-  
-  # calculate adjacency matrix from voronoi cells for stations
-  return(spdep::nb2mat(spdep::poly2nb(vor), style = "B", zero.policy = TRUE))
-}
-
 adj_mat <- calc_adj_mat(pts, plot = TRUE)
+# save
+saveRDS(adj_mat, "data/ire_adj_mat.RDS")
 
 #### Simple k-mediod cluster on a and b values ####
 
@@ -162,50 +113,12 @@ ab_mat <- as.matrix(ab_mat)
 dist_mat_euc <- dist(ab_mat, method = "Euclidean")
 
 # fit PAM for k = 1:10
-scree_plot <- \(dist_mat) {
-  total_within_ss <- vapply(
-    1:10, within_cluster_sum, dist_mat, FUN.VALUE = numeric(1)
-  )
-  
-  # scree plot
-  plot(
-    1:10, total_within_ss, type = "b", pch = 19 # , 
-    # xlab = "Number of Clusters (k)", 
-    # ylab = "Total Within-Cluster Sum of Distances",
-    # main = "Scree Plot for K-medoids Clustering"
-  )
-  return(total_within_ss)
-}
 scree_plot(dist_mat_euc)
 
 # Seems like there's a very slight elbow at k = 4
 pam_euclid_clust <- pam(dist_mat_euc, k = 4)
 
-# plot 
-#  Have different size/shape for cluster mediods
-plt_clust <- \(pts, pam_clust) {
-  medoids <- pam_clust$medoids
-  if (inherits(medoids, "matrix")) medoids <- as.numeric(rownames(medoids))
-  # browser()
-  pts_plt <- cbind(pts, data.frame("clust" = pam_clust$clustering)) %>% 
-    mutate(row = row_number())
-  pts_plt$mediod <- ifelse(
-    pts_plt$row %in% medoids, TRUE, FALSE
-  )
-
-  ggplot(areas) + 
-    geom_sf(colour = "black", fill = NA) + 
-    geom_sf(
-      data = pts_plt, 
-      aes(colour = factor(clust), shape = mediod, size = as.numeric(mediod)), 
-      alpha = 0.8
-    ) + 
-    scale_shape_discrete(breaks = c(1, 15)) + 
-    scale_size_continuous(range = c(3, 6)) +  
-    guides(shape = "none", size = "none") + 
-    theme
-}
-
+# plot
 plt_clust(pts, pam_euclid_clust)
 
 
@@ -227,18 +140,8 @@ plt_clust(pts, pam_euc_adj_clust)
 
 #### Vignotto 2021 KL divergence clustering ####
 
-# function to convert to Pareto scale
-# divide by N + 1, not N (so do i / n + 1)
-# x: Vector representing e.g. rain
-pareto_trans <- \(x) {
-  stopifnot(is.vector(x))
-  # calculate ECDF
-  ecdf_vals <- order(x) / (length(x) + 1) 
-  # pareto transform
-  return(1 / (1 - ecdf_vals))
-}
-
-# TODO: Investigate why no observations appear to be extreme for both???
+# TODO: For pareto_transform, investigate why no observations appear to be 
+# extreme for both???
 # nrows for first place
 data %>% 
   filter(name == data$name[[1]]) %>% 
@@ -256,149 +159,9 @@ data.frame(
   nrow() # 4
 # None == 0, so why does this not carry through to below??
 
-# function to compute bivariate risk function
-# x: list of vectors representing different 
-# fun: fun used to calculate "risk", defaults to max as easier to partition
-risk_fun <- \(x, fun = max) {
-  # test list
-  stopifnot(is.list(x)) 
-  # test equal length
-  stopifnot(length(unique(vapply(x, length, numeric(1)))) == 1) 
-  
-  # TODO: Improve/speedup (could have x as a dataframe!)
-  risk_vec <- vector(length = length(x[[1]]))
-  for (i in seq_along(x[[1]])) {  
-    risk_vec[[i]] <- fun(
-      # pull ith entry in each vector in the list x
-      vapply(x, `[[`, i, FUN.VALUE = numeric(1))
-    )
-  }
-  return(risk_vec)
-}
-
-# Partition into 3 subsets, calculate empirical proportion in each
-# TODO: Only works for max risk fun, unsure how to do for sum...
-# TODO: Fix, obviously wrong since it doesn't identify times where both 
-# are high!
-partition_max <- \(x, prob, plot = FALSE) {
-  
-  # x must be a list of length 2 (rain and wind speed)
-  stopifnot(is.list(x) && length(x) == 2) 
-  
-  # convert to dataframe, easier to subset (must follow this order!)
-  df <- data.frame(
-    rain       = x[[1]], 
-    wind_speed = x[[2]], # TODO: Make x a dataframe already
-    # TODO: Fix this, wrong somehow!!!
-    R          = risk_fun(x, max)
-  ) 
-  
-  # calculate quantile of risk function
-  qu <- quantile(df$R, prob = prob)[[1]]
-  
-  # partition into 3 subsets
-  df <- df %>% 
-    mutate(extreme = case_when(
-      rain > qu & wind_speed > qu ~ "both",
-      rain > qu                    ~ "rain",
-      wind_speed > qu              ~ "wind_speed",
-      TRUE                          ~ NA
-    ))
-  
-  if (plot) {
-    df %>% 
-      ggplot() +
-      geom_point(aes(x = rain, y = wind_speed, colour = extreme)) + 
-      geom_vline(xintercept = qu, linetype = "dashed") + 
-      geom_hline(yintercept = qu, linetype = "dashed") + 
-      ggsci::scale_colour_nejm() + 
-      labs(y = "wind speed", title = paste0(
-        "Hazard subsets, q_u = ", round(qu, 3), " (prob = ", prob, ")"
-      )) + 
-      theme
-  }
-  
-  # return list with points falling into each category
-  return(list(
-      "rain"       = sum(df$extreme == "rain", na.rm = TRUE),
-      "wind_speed" = sum(df$extreme == "wind_speed", na.rm = TRUE),
-      "both"       = sum(df$extreme == "both", na.rm = TRUE)
-    )
-  )
-}
-
 # TODO: re colour points at original scale
 
-# Function to calculate proportion 
-calc_prop <- \(x) {
-  stopifnot(is.list(x) & names(x) == c("rain", "wind_speed", "both"))
-  # denom <- length(unlist(x))
-  denom <- sum(unlist(x))
-  ret <- lapply(x, \(y) y / denom)
-  names(ret) <- names(x)
-  return(ret)
-}
-
-# function to calculate KL divergence between any two locations
-emp_kl_div <- \(x, y, convert_pareto = TRUE, prob = 0.9, print = TRUE) {
-  # stopifnot(length(x) == length(y))
-  
-  # split x and y in half (rain vs wind speed)
-  # remove NAs from x and y from padding matrix
-  x <- x[!is.na(x)]
-  y <- y[!is.na(y)]
-  n <- length(x)
-  m <- length(y) # need for m???
-  df_lst <- list(
-  # x_df <- data.frame(
-    "x" = data.frame(
-      "rain"       = x[1:(n / 2)],
-      "wind_speed" = x[((n / 2) + 1):n]
-    ),
-    "y" = data.frame(
-    "rain"       = y[1:(m / 2)],
-    "wind_speed" = y[((m / 2) + 1):m]
-    )
-  )
-   
-  # convert to Pareto scale
-  df_lst_par <- df_lst
-  if (convert_pareto) {
-    df_lst_par <- lapply(df_lst, \(z) {
-      data.frame(apply(z, 2, pareto_trans))
-    })
-  }
-  
-  # partition into 3 subsets
-  df_part <- lapply(df_lst_par, \(z) {
-    partition_max(list(z[, 1], z[, 2]), prob = prob)
-  })
-  
-  # calculate proportions of partitions
-  # TODO: parts of df part can just be named vector, doesn't need list!
-  df_part_prop <- lapply(df_part, \(z) {
-    denom <- sum(unlist(z)) # denominator is # extreme obs
-    lapply(z, `/`, denom) # find prop of extreme obs for each disjoint set
-  })
-  
-  # calculate proportions of partitions
-  x_part <- df_part_prop[[1]]
-  y_part <- df_part_prop[[2]]
-  if (print) {
-    print(paste0("both for x: ", x_part$both))
-    print(paste0("both for y: ", round(y_part$both)))
-  }
-  sum_vals <- vector(length = length(x_part))
-  for (i in seq_along(sum_vals)) { 
-    sum_vals[i] <- (x_part[[i]] - y_part[[i]]) * 
-                      log(x_part[[i]] / y_part[[i]])
-  }
-  # sum_vals[is.nan(sum_vals) | is.infinite(sum_vals)] <- 0
-  if (any(is.nan(sum_vals) | is.infinite(sum_vals), na.rm = TRUE)) return(NA)
-  return((1  / 2) * sum(sum_vals))
-}
-
-# test for 2 locations
+# test emp_kl_div for 2 locations
 emp_kl_div(
   c(data$rain[data$name == data$name[[1]]], data$wind_speed[data$name == data$name[[1]]]),
   # c(data$rain[data$name == "Portumna O.p.w."], data$wind_speed[data$name == "Portumna O.p.w."]),
@@ -443,21 +206,21 @@ data_lst <- data %>%
   # split data by location
   group_split(name) %>% 
   purrr::map(\(x) x %>% 
-               select(rain, wind_speed) %>% 
+               dplyr::select(rain, wind_speed) %>% 
                stack() %>% 
-               select(1) %>% 
+               dplyr::select(1) %>% 
                as.vector() %>% 
                `[[`(1))
 
 # Calculate dissimilarity matrix 
 # TODO: Doesn't work for p = 0.9, some sites have no doubly extreme obs
-prob <- 0.8
+prob <- 0.99
 kl_mat <- dist(data_lst, method = emp_kl_div, print = FALSE, prob = prob)
 
 # check for NAs, reduce prob if required
 (nas <- apply(apply(kl_mat, 2, is.na), 2, sum))
 while (any(nas) > 0) {
-  prob <- prob - 0.5
+  prob <- prob - 0.05
   print(paste0("prob = ", prob))
   kl_mat <- dist(data_lst, method = emp_kl_div, print = FALSE, prob = prob)
 }

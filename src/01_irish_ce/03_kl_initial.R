722cdf526358f3e09e0ddc08761e0f4f68877bd3
paddy7wb <paddy7wb@gmail.com>
2024-10-29 14:45:37 +0000

    Initial implementation of KL, using samples from normal dist

--- src/01_irish_ce/03_kl_initial.R
+++ src/01_irish_ce/03_kl_initial.R
@@ -47,11 +47,58 @@ sf::sf_use_s2(FALSE)
 
 #### Support functions ####
 
+# TODO: Add to their own file!
+
 # compute the total within-cluster sum of distances
 within_cluster_sum <- function(k, distance_matrix) {
   kmedoids_result <- pam(distance_matrix, k = k)
   return(kmedoids_result$objective[1])  # Total cost (sum of distances)
 }
+
+# calculate scree plot
+scree_plot <- \(dist_mat, k_vec = 1:10) {
+  
+  total_within_ss <- vapply(
+    k_vec, within_cluster_sum, dist_mat, FUN.VALUE = numeric(1)
+  )
+  
+  # scree plot
+  plot(
+    k_vec, 
+    total_within_ss, 
+    type = "b", 
+    xlab = "Number of Clusters (k)", 
+    ylab = "Total Within-Cluster Sum of Distances",
+    main = "Scree Plot for K-medoids clustering",
+    pch = 19 
+  )
+  return(total_within_ss)
+}
+
+# plot clustering solution on map
+plt_clust <- \(pts, pam_clust) {
+  medoids <- pam_clust$medoids
+  if (inherits(medoids, "matrix")) medoids <- as.numeric(rownames(medoids))
+  # browser()
+  pts_plt <- cbind(pts, data.frame("clust" = pam_clust$clustering)) %>% 
+    mutate(row = row_number())
+  pts_plt$mediod <- ifelse(
+    pts_plt$row %in% medoids, TRUE, FALSE
+  )
+
+  ggplot(areas) + 
+    geom_sf(colour = "black", fill = NA) + 
+    geom_sf(
+      data = pts_plt, 
+      aes(colour = factor(clust), shape = mediod, size = as.numeric(mediod)), 
+      alpha = 0.8
+    ) + 
+    scale_shape_discrete(breaks = c(1, 15)) + 
+    scale_size_continuous(range = c(3, 6)) +  
+    guides(shape = "none", size = "none") + 
+    theme
+}
+
 
 #### Load Data ####
 
@@ -68,6 +115,11 @@ dependence <- readRDS("data/texmex_mexdep_obj.RDS")
 # a and b values
 ab_df <- readr::read_csv("data/ab_vals.csv")
 # ab_df <- readr::read_csv("data/ab_vals_fixed_b.csv")
+
+# extract point location of each station for plotting on map
+pts <- ab_df %>% 
+  distinct(lon, lat) %>% 
+  st_to_sf()
 
 # remove locations with NAs for a
 na_locs <- ab_df %>% 
@@ -95,7 +147,8 @@ ab_df_wide <- ab_df %>%
   select(name, county, col, value) %>% 
   pivot_wider(names_from = col, values_from = value)
 
-#### Functions ####
+
+#### Functions and testing ####
 
 # pull a, b, mu, sigma for a given location
 # dep - List of mex objects (i.e. CE models for each var) for single location
@@ -117,6 +170,7 @@ pull_params <- \(dep) {
 }
 
 # pull parameter values for each location
+# TODO: Easier to do as dataframe than in lists?
 params <- lapply(dependence, pull_params)
 
 # pull thresholds
@@ -126,7 +180,8 @@ pull_thresh_trans <- \(dep) {
   stopifnot(is.list(dep))
   stopifnot(all(vapply(dep, class, character(1)) == "mex"))
   # return quantile of transformed data (already calculated in texmex)
-  return(vapply(dep, \(x) x$dependence$dth, numeric(1)))
+  # return(vapply(dep, \(x) x$dependence$dth, numeric(1)))
+  return(lapply(dep, \(x) x$dependence$dth))
 }
 
 # pull Laplace threshold values for each location
@@ -134,9 +189,163 @@ thresh <- lapply(dependence, pull_thresh_trans)
 
 # take maximum Laplace thresholds; want to geenra
 thresh_max <- apply(bind_rows(thresh), 2, max)
+# thresh_max <- as.list(apply(bind_rows(thresh), 2, max))
 
-# Calculate 
-to_name <- \(params, thresh_sup, n = 1000) {
-  rnorm(n, )
+# Function to sample from normal dist (likelihood for single conditional) for 
+# Y_{-i}|Y_{i} for data exceeding global threshold
+# TODO: Check normal distribution sampled from correctly, particularly logic
+# params - CE parameters for one location
+# thresh - Thresholds for one location
+# thresh_max - Global maximum threshold
+# data_max - Maximum data point to go up to, defaults as twice thresh_max
+# data_len - How many data points between thresh_max and data_max should we 
+# sample from a normal distribution for?
+# n - Number of normal samples to generate for each data point
+gen_ce_norm <- \(
+  params, thresh, thresh_max, data_max = 2 * thresh_max, data_len = 10, n = 1000
+) {
+  # print(data_max)
+  # print(data_len)
+  # parameters
+  # TODO: Can pull in a more programmatic way
+  a <- params[["a"]] # alpha 
+  b <- params[["b"]] # beta
+  m <- params[["m"]] # mu nuisance parameter
+  s <- params[["s"]] # sigma nuisance
+  
+  # Take sequence of length data_len from global max thresh to specified max
+  # TODO: Should we sample from this using `sample()`?? Wouldn't be the same 
+  # across different locations then, unless we did outside of function and/or 
+  # had same seed
+  data <- seq(thresh_max, data_max, length = data_len)
+  
+  # generate normal samples for each data point
+  return(vapply(data, \(x) {
+    # Likelihood for single conditional (5.2 H & T 2004)
+    rnorm(
+      n     = n,
+      # mean  = a * x + m * (x ^ b),
+      mean  = a * x + m * (x ^ b), #  + 10, # shift to the right -> non-neg
+      sd    = s * (x ^ b)
+    )
+  }, FUN.VALUE = numeric(n)))
 }
 
+set.seed(123)
+gen_ce_norm(
+  params = params[[1]]$rain, 
+  thresh = thresh[[1]][["rain"]], 
+  thresh_max = thresh_max[["rain"]]
+)
+
+set.seed(123)
+x <- gen_ce_norm(
+  params = params[[1]]$rain, 
+  thresh = thresh[[1]][["rain"]], 
+  thresh_max = thresh_max[["rain"]]
+)
+
+set.seed(123)
+y <- gen_ce_norm(
+  params = params[[2]]$rain, 
+  thresh = thresh[[2]][["rain"]], 
+  thresh_max = thresh_max[["rain"]]
+)
+
+# Compare normal samples using KL divergence
+# TODO: Could functionalise much of this??
+# x - Matrix 
+kl_compare <- \(x, y) {
+  
+  # convert x and y from matrix to vector form, if required
+  x_fun <- x
+  y_fun <- y 
+  if (is.matrix(x)) {
+    x_fun <- as.vector(x)
+  }
+  if (is.matrix(y)) {
+    y_fun <- as.vector(y)
+  }
+  stopifnot(is.vector(x_fun) && is.vector(y_fun))
+  
+  # shift upwards if required, won't change shape of distribution
+  # TODO: However, does appear to effect value of KL divergence!
+  if (any(x_fun < 0) || any(y_fun < 0)) {
+    epsilon <- 1E-8
+    min_val <- min(c(x_fun, y_fun)) - epsilon
+    message(paste0("Shifting by ", min_val, " so samples are non-negative"))
+    # min_val <- -5 + epsilon
+    # min_val <- -10 + epsilon
+    x_fun <- x_fun - min_val
+    y_fun <- y_fun - min_val
+  }
+  
+  # divide by sum to get proper distributions
+  x_fun <- x_fun / sum(x_fun)
+  y_fun <- y_fun / sum(y_fun)
+  
+  plot(
+    density(x_fun, bw = 3.807e-6), 
+    xaxt = "n", 
+    yaxt = "n", 
+    xlim = c(-0.00005, 0.00025), 
+    ylim = c(0, 40000)
+  )
+  # axis(1, at = seq(0, 0.00020, by = 0.00005))
+  axis(1, at = seq(0, 0.00020, by = 0.0001))
+  axis(2, at = seq(0, 40000, by = 10000))
+  lines(density(y_fun), col = "red")
+   
+  # calculate JS divergence between x and y (symmetric and a proper metric)
+  # 
+  return(philentropy::distance(
+    rbind(x_fun, y_fun), 
+    # method = "kullback-leibler", 
+    method = "jensen-shannon", 
+    unit = "log", 
+    mute.message = TRUE
+  ))
+}
+
+#### Clustering ####
+
+# for each location, generate normal samples
+norm_samples_loc <- lapply(seq_along(params), \(i) {
+  lapply(seq_along(params[[i]]), \(j) {
+    gen_ce_norm(
+      params     = params[[i]][[j]], 
+      thresh     = thresh[[i]][[j]],
+      thresh_max = thresh_max[[j]]
+    )
+  })
+})
+
+# pull for rain and wind speed
+# norm_samples_rain <- lapply(norm_samples, \(x) as.vector(x[[1]]))
+# norm_samples_ws <- lapply(norm_samples, \(x) as.vector(x[[2]]))
+
+# list of locs -> list of len 2 of variables, each containing all locs
+norm_samples_var <- purrr::transpose(norm_samples_loc)
+
+# calculate distance matrices
+# dist_rain <- dist(x = norm_samples_rain, method = kl_compare)
+# dist_ws <- dist(x = norm_samples_rain, method = kl_compare)
+dist_mats <- lapply(norm_samples_var, dist, method = kl_compare)
+
+# scree plots
+lapply(dist_mats, scree_plot)
+
+# cluster based on both
+# TODO: Not working at all! Need to fix
+plt_clust(pts, pam(dist_mats[[2]], k = 2, diss = TRUE)) 
+
+# cluster adjacent sites only
+dist_mats_adj <- lapply(dist_mats, \(x) {
+  ret <- as.matrix(x)
+  ret[adj_mat == 0] <- 1e9
+  return(ret)
+})
+
+lapply(dist_mats_adj, scree_plot)
+
+plt_clust(pts, pam(dist_mats_adj[[1]], k = 3, diss = FALSE)) 

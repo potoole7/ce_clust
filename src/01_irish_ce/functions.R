e87bac1fd449d2309dc9f627c81587ad19f02164
paddy7wb <paddy7wb@gmail.com>
2024-11-04 18:06:13 +0000

    Move Jensen-Shannon functions to functions file

--- src/01_irish_ce/functions.R
+++ src/01_irish_ce/functions.R
@@ -1,5 +1,94 @@
 #### Functions ####
 
+#### Closed form normal-normal KL/Jensen-Shannon Divegence ####
+
+# pull a, b, mu, sigma for a given location
+# dep - List of mex objects (i.e. CE models for each var) for single location
+pull_params <- \(dep) {
+  # fail if not list of mex objects for single location
+  stopifnot(is.list(dep))
+  stopifnot(all(vapply(dep, class, character(1)) == "mex"))
+  # loop through conditioning variables for single location
+  return(lapply(dep, \(x) {
+    # pull parameter vals
+    ret <- as.vector(x$dependence$coefficients)
+    names(ret) <- rownames(x$dependence$coefficients)
+    # remove c and d if 0 (i.e. Laplace margins, rather than Gumbel)
+    if (ret["c"] == 0 && ret["d"] == 0) {
+      ret <- ret[!names(ret) %in% c("c", "d")]
+    } 
+    return(ret)
+  }))
+}
+
+# pull thresholds
+# dep - List of mex objects (i.e. CE models for each var) for single location
+pull_thresh_trans <- \(dep) {
+  # fail if not list of mex objects for single location
+  stopifnot(is.list(dep))
+  stopifnot(all(vapply(dep, class, character(1)) == "mex"))
+  # return quantile of transformed data (already calculated in texmex)
+  return(lapply(dep, \(x) x$dependence$dth))
+}
+
+# function to calculate KL divergence for single data point
+# derivation at https://statproofbook.github.io/P/norm-kl.html
+kl_gauss <- \(mu1, mu2, var1, var2) {
+  return(1 / 2 * (
+    (mu2 - mu1)^2 / var2 + (var1 / var2) - log(var1 / var2) - 1
+  ))
+}
+
+# function to calculate Jensen-Shannon divergence metric from KL divergence
+# This metric is symmetric, as required for clustering 
+# https://tinyurl.com/526rwy9f
+js_gauss <- \(mu1, mu2, var1, var2) {
+  # calculate mean, variance for mixture distribution M = (P + Q)/2
+  # Sum of normals as in https://online.stat.psu.edu/stat414/lesson/26/26.1
+  # (N(u1, v1) + N(u2, v2)) / 2 = N((u1 + u2) / 2, (v1 + v2) / 2^2)
+  mu_m <- (mu1 + mu2) / 2 
+  var_m <- (var1 + var2) / 4
+  
+  # calculate JS(P||Q) = ((KL(P||M)) + KL(Q||M))/2
+  return(
+    (kl_gauss(mu1, mu_m, var1, var_m) + kl_gauss(mu2, mu_m, var1, var_m)) / 2
+  )
+}
+
+# Function to calculate Jensen-Shannon divergence for each data point
+js_div <- \(params_x, params_y, thresh_max, data_max = 2 * thresh_max, n_dat) {
+  
+  # test that input vectors have correct conditional extremes parameters
+  stopifnot(is.vector(params_x) && is.vector(params_y))
+  stopifnot(
+    all(c(names(params_x), names(params_y)) == rep(c("a", "b", "m", "s"), 2))
+  )
+  
+  # create data sequence from specified arguments
+  data <- seq(thresh_max, data_max, length = n_dat)
+  
+  # funs to calculate mu and sd for normal dist as in 5.2 of Heff & Tawn '04
+  mu_fun <- \(x, data) {
+    return(x[["a"]] * data + x[["m"]] * (data ^ x[["b"]]))
+  }
+  var_fun <- \(x, data) {
+    # take square now to avoid in kl_single formula
+    return((x[["s"]] * (data ^ x[["b"]]))^2)
+  }
+  
+  # calculate mu and sigma for each data point
+  mus <- lapply(list(params_x, params_y), mu_fun, data = data)
+  vars <- lapply(list(params_x, params_y), var_fun, data = data)
+  
+  # Calculate Jensen-Shannon divergence for each data point
+  # TODO: How best to summarise across all data points? Sum? Average?
+  return(sum(mapply(
+    js_gauss,
+    mu1 = mus[[1]], mu2 = mus[[2]], var1 = vars[[1]], var2 = vars[[2]]
+  )))
+}
+
+
 #### Vignotto 2021 ####
 
 # function to convert to Pareto scale

879120ee8abac0b6e57311f6ecd57cd0cd28554b
paddy7wb <paddy7wb@gmail.com>
2024-11-22 17:19:09 +0000

    Test evc package by clustering via both methods

---
+++ src/00_test_evc_pkg/00_test_ce.R
@@ -0,0 +1,41 @@
+#### Test evc package functions to see if they're working ####
+
+# Rough test of package functions
+
+#### Libs ####
+
+library(dplyr)
+library(evc)
+
+# source("src/01_irish_ce/functions.R")
+
+#### Load Data ####
+
+data_mix <- readRDS("data/sim_dat_3_clust.RDS")
+cluster_mem <- c(1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3)
+
+#### Vignotto 2021 ####
+
+kl_sim_eval(
+  data_mix, 
+  kl_prob     = 0.9, 
+  k           = 3, 
+  cluster_mem = cluster_mem 
+)
+
+#### Fit CE model and cluster ####
+
+dependence <- fit_ce(
+  data_mix, 
+  marg_prob   = 0.9,
+  cond_prob   = 0.9,
+  split_data  = TRUE 
+)
+
+js_clust(
+  dependence,  
+  nclust = 3, 
+  cluster_mem = cluster_mem
+)
+
+#### Plotting ####

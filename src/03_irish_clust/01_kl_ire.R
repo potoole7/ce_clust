e0610534c1e5650eb80b5a6d11c5c28551a9d77d
paddy7wb <paddy7wb@gmail.com>
2024-12-02 15:31:10 +0000

    Additions to plot

--- src/03_irish_clust/01_kl_ire.R
+++ src/03_irish_clust/01_kl_ire.R
@@ -1,9 +1,4 @@
-#### Conditional Extremes clustering for Irish data ####
-
-# Note: Doesn't work for fixed b when keeping b values, so remove
-
-# TODO: Investigate differences in length for reglines
-# TODO: Get data for 6 counties
+#### Investigatory and Vignotto 2021 clustering on Irish data ####
 
 #### Libs ####
 
@@ -51,125 +46,125 @@ sf::sf_use_s2(FALSE)
 
 # load original dataset
 data <- readr::read_csv("data/met_eireann/final/met_eir_preprocess.csv.gz")
-# find rough mean and variance of bulk rain and wind data across Ireland
-data %>% 
-  filter(rain != 0, wind_speed != 0) %>% 
-  filter(rain < quantile(rain, 0.9), wind_speed < quantile(wind_speed, 0.9)) %>% 
-  reframe(across(c("rain", "wind_speed"), \(x) c(mean(x), var(x))))
 
 # load shapefile
 areas <- sf::read_sf("data/met_eireann/final/irl_shapefile.geojson")
 
-# Fitted conditional extremes models for each site
-dependence <- readRDS("data/texmex_mexdep_obj.RDS")
-# sort alphabetically by name
-dependence <- dependence[sort(names(dependence))]
-# dependence <- readRDS("data/texmex_mexdep_obj_fixed_b.RDS")
-
-# a and b values
-ab_df <- readr::read_csv("data/ab_vals.csv") %>% 
+# a and b values from CE model (for simple clustering)
+ab_df <- readr::read_csv("data/ab_vals.csv") %>%
   arrange(name)
 # ab_df <- readr::read_csv("data/ab_vals_fixed_b.csv")
 
-# extract point location of each station for plotting on map
-pts <- ab_df %>% 
-  distinct(lon, lat) %>% 
-  st_to_sf()
-
 # remove locations with NAs for a
-na_locs <- ab_df %>% 
-  filter(is.na(value)) %>% 
-  pull(name) %>% 
+na_locs <- ab_df %>%
+  filter(is.na(value)) %>%
+  pull(name) %>%
   unique()
 
 if (length(na_locs) > 0) {
   ab_df <- filter(ab_df, !name %in% na_locs)
-  dependence <- dependence[
-    !grepl(na_locs, names(dependence), fixed = TRUE)
-  ]
 }
 
-# load adjacency matrix
-adj_mat <- readRDS("data/ire_adj_mat.RDS")
-
 # remove locations with NAs from data
-data <- data %>% 
+data <- data %>%
   semi_join(ab_df, by = "name")
 
 # convert ab values to wide format
-ab_df_wide <- ab_df %>% 
-  mutate(col = paste0(var, "_", parameter)) %>% 
-  dplyr::select(name, county, col, value) %>% 
+ab_df_wide <- ab_df %>%
+  mutate(col = paste0(var, "_", parameter)) %>%
+  select(name, county, col, value) %>%
   pivot_wider(names_from = col, values_from = value)
 
 
-#### Clustering ####
+#### Calculate Voronoi cells and adjacency matrix ####
 
-# Perform PAM and k-means clustering, as an exploratory analysis
+# extract point location of each station
+pts <- ab_df %>%
+  distinct(lon, lat) %>%
+  st_to_sf()
 
-
-# pull parameter values for each location
-params <- lapply(dependence, pull_params)
-
-# pull Laplace threshold values for each location
-thresh <- lapply(dependence, pull_thresh_trans)
-
-# take maximum Laplace thresholds; want to geenra
-# thresh_max <- apply(bind_rows(thresh), 2, max)
-thresh_max <- lapply(bind_rows(thresh), max)
-
-# list of locs -> list of len 2 of variables, each containing all locs
-params <- purrr::transpose(params)
-
-dist_mats <- lapply(seq_along(params), \(i) {
- proxy::dist(
-   params[[i]], 
-   method = js_div, 
-   thresh_max = thresh_max[[i]], 
-   data_max = 2 * thresh_max[[i]], 
-   n_dat = 10
- )
-})
-
-# Sum distance matrices for different variables
-dist_mat <- do.call(`+`, dist_mats)
-
-# scree plots
-scree_plot(dist_mat)
-scree_plot(dist_mat, fun = kmeans)
-
-# plot clustering for both rain and wind speed
-# ire_clust_plots <- lapply(dist_mats, \(x) {
-#   # for rain or wind speed, plot clustering based on PAM and k-means
-#   lapply(c(pam, kmeans), \(fun) {
-#     plt_clust_map(pts, fun(x, 3))
-#   })
-# })
-# TODO: Any way to plot how likely points are to be in other clusters?
-# Silhoutte plot??
-ire_clust_plots <- lapply(c(pam, kmeans), \(fun) {
-    plt_clust_map(pts, fun(dist_mat, 3))
-})
-
-pam_clust2 <- kl_sim_eval(
-  dependence, kl_prob = 0.9, k = 2, dist_mat = dist_mat
-)
-plt_clust_map(pts, areas, pam_clust2)
-
-# cluster adjacent sites only
-#dist_mats_adj <- lapply(dist_mats, \(x) {
-#  ret <- as.matrix(x)
-#  ret[adj_mat == 0] <- 1e9
-#  return(ret)
-#})
-dist_mat_adj <- as.matrix(dist_mat)
-dist_mat_adj[adj_mat == 0] <- 1e9
-
-scree_plot(dist_mat_adj)
-scree_plot(dist_mat_adj, fun = kmeans)
-
-ire_clust_adj_plots <- lapply(c(pam, kmeans), \(fun) {
-    plt_clust_map(pts, fun(dist_mat_adj, 3))
-})
+# calculate adjacency matrix from Voronoi cells
+adj_mat <- calc_adj_mat(pts, plot = TRUE)
+# save
+saveRDS(adj_mat, "data/ire_adj_mat.RDS")
 
 
+#### Simple k-mediod cluster on a and b values ####
+
+# matrix of a and b values for rain and wind for each site
+ab_mat <- ab_df_wide
+# if all b values are "fixed", only cluster on a values
+if (all(ab_df_wide$rain_b == ab_df_wide$rain_b[1])) {
+  ab_mat <- select(ab_mat, contains("_a"))
+} else {
+  ab_mat <- select(ab_mat, contains("_a"), contains("_b"))
+}
+ab_mat <- as.matrix(ab_mat)
+
+# dissimilarity matrix, by euclidean distance (simple, prob incorrect approach)
+dist_mat_euc <- dist(ab_mat, method = "Euclidean")
+
+# Elbow and silhouette plotting to determine number of clusters
+scree_plot(dist_mat_euc) # indicates 2 has slight elbow
+sil_boxplot(dist_mat_euc) # looks like 2 as well 
+
+# plot
+plt_clust_map(pts, areas, pam(dist_mat_euc, k = 2))
+plt_clust_map(pts, areas, pam(dist_mat_euc, k = 3))
+
+
+#### Cluster adjacent sites only ####
+
+# apply adjacency matrix to dissimilarity matrix
+dist_euc_adj <- as.matrix(dist_mat_euc)
+dist_euc_adj[adj_mat == 0] <- 1e9
+
+# scree plot
+scree_plot(dist_euc_adj)
+sil_boxplot(dist_euc_adj) # seems to choose 6
+
+# TODO: Fix, ordering (or adjacency) seems wrong
+plt_clust_map(pts, areas, pam(dist_euc_adj, k = 2))
+plt_clust_map(pts, areas, pam(dist_euc_adj, k = 6))
+
+
+#### Vignotto 2021 KL divergence clustering ####
+
+# Split data into lists for each location as required by emp_kl_div
+data_lst <- data %>%
+  # split data by location
+  group_split(name) %>%
+  purrr::map(\(x) x %>%
+               dplyr::select(rain, wind_speed) %>%
+               stack() %>%
+               dplyr::select(1) %>%
+               as.vector() %>%
+               `[[`(1))
+
+# scree plot, looks like k = 3
+prob <- 0.88 # fails for prob <- 0.9
+# TODO: Investigate location?
+kl_mat <- kl_sim_eval(data_lst, prob, k = NULL)$dist_mat
+
+scree_plot(kl_mat, k = 1:5)
+sil_boxplot(kl_mat, k = 2:5)
+
+# plot results of clustering
+plt_clust_map(pts, areas, kl_sim_eval(data_lst, prob, k = 2, kl_mat))
+plt_clust_map(pts, areas, kl_sim_eval(data_lst, prob, k = 3, kl_mat))
+
+plot(silhouette(kl_sim_eval(data_lst, prob, k = 2, kl_mat)))
+plot(silhouette(kl_sim_eval(data_lst, prob, k = 3, kl_mat)))
+
+# imposing adjacency
+kl_mat_adj <- as.matrix(kl_mat)
+kl_mat_adj[adj_mat == 0] <- 1e9
+
+scree_plot(kl_mat_adj, k = 1:8)
+sil_boxplot(kl_mat_adj, k = 1:8) # suggests k = 6
+
+# plot results of clustering
+# TODO: Fix, points don't look adjacent at all!
+plt_clust_map(pts, areas, kl_sim_eval(data_lst, prob, k = 2, kl_mat_adj))
+plt_clust_map(pts, areas, kl_sim_eval(data_lst, prob, k = 3, kl_mat_adj))
+plt_clust_map(pts, areas, kl_sim_eval(data_lst, prob, k = 6, kl_mat_adj))

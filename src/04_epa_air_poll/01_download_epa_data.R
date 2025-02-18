9ac8ea474355be12781d20f9d9a91566daf1cf9c
paddy7wb <paddy7wb@gmail.com>
2025-02-18 10:39:33 +0000

    Add CO, O3, NO to data

--- src/04_epa_air_poll/01_download_epa_data.R
+++ src/04_epa_air_poll/01_download_epa_data.R
@@ -18,6 +18,9 @@ library(RAQSAPI)
 library(lubridate)
 library(dplyr)
 library(parallel)
+library(ggplot2)
+
+theme <- evc::evc_theme()
 
 
 #### Metadata ####
@@ -26,33 +29,54 @@ email <- "pot23@bath.ac.uk"
 pass <- "taupefox76" # TODO: Have as environment variable!!
 
 # parameter numbers for various pollutants
-# TODO: Some parameters not coming through? Remove Carbon Monoxide
 params <- c(
   "PM1"   = "81102",
   "PM2.5" = "88101",
   "PM10"  = "81103",
   "SO2"   = "42401",
-  "NO"    = "42602",
   "CO"    = "42101",
-  "HC"    = "45201" # Benzene
+  "HC"    = "45201",
+  "NO2"   = "42601",
+  "NO"    = "42602", 
+  "O3"    = "44201"
 )
+
+# params with poor records for Buffalo
+# params <- c(
+#   "PM2.5" = "88101",
+#   "HC"    = "45201" # Benzene
+# )
+
+# missing params
+# params <- c(
+#   # Carbon monoxide
+#   "CO"    = "42101",
+#   # Ozone
+#   "O3"    = "44201"
+#   # Nitric Oxide
+#   "NO"    = "42602"
+# )
 
 # state numeric codes for cities of interest
 # TODO: Could change to be a key?
 state_codes <- list(
   # Rust Belt
-  "MD" = "24", "NY" = "36", "IL" = "17", "OH" = "39", 
+  "MD" = "24", "NY" = "36", "IL" = "17", "OH" = "39",
   "MI" = "26", "PA" = "42", "NY" = "36", "MO" = "29",
   # West Coast
-  "CA" = "06", "WA" = "53", "OR" = "41", "CA" = "06", "CA" = "06", "CA" = "06",
+  # "CA" = "06", "WA" = "53", "OR" = "41", "CA" = "06", "CA" = "06", "CA" = "06",
+  "CA" = "06", "WA" = "53", "CA" = "06", "CA" = "06", "OR" = "41", "CA" = "06",
   # Texas/South
   "TX" = "48", "TX" = "48", "TX" = "48", "TX" = "48", "LA" = "22", "NM" = "35"
 )
 
 # Temp: Add missing cities
-state_codes <- list(
-  "CA" = "06", "OR" = "41", "MO" = "29"
-)
+# state_codes <- list(
+#   "CA" = "06", "OR" = "41", "MO" = "29"
+# )
+
+# Buffalo
+# state_codes <- list("NY" = "36")
 
 # countycodes for cities
 county_codes <- list(
@@ -67,17 +91,24 @@ county_codes <- list(
   "New Orleans" = "071", "Albuquerque" = "001"
 )
 
+# Buffalo
+# county_codes <- list("Buffalo" = "029")
+
 # Temp: just do missing cities
-county_codes <- list(
-  "Los Angeles" = "037", "Portland" = "051", "St. Louis" = "510"
-)
+# county_codes <- list(
+#   "Los Angeles" = "037", "Portland" = "051", "St. Louis" = "510"
+# )
 
 # start and end dates 
 end_date <- as_date("2024/01/01")
-start_date <- as_date("1980/01/01") # from Minnesota paper
+# end_date <- as_date("2015/01/01") # Benzene Buffalo
+# end_date <- as_date("2001/01/01") # PM2.5 Buffalo
 
-ncores <- min(detectCores() - 1, length(params))
-# ncores <- detectCores() - 3
+# start_date <- as_date("1980/01/01") 
+start_date <- as_date("1990/01/01") 
+
+# ncores <- min(detectCores() - 1, length(params))
+ncores <- detectCores() - 3
 
 
 #### Main ####
@@ -88,31 +119,27 @@ ncores <- min(detectCores() - 1, length(params))
 aqs_credentials(username = email, key = pass)
 
 # pull daily summaries for each pollutant and county
-data <- mclapply(names(params), \(param) {
-# data <- lapply(names(params), \(param) {
+# data <- mclapply(names(params), \(param) {
+data <- lapply(names(params), \(param) {
   lapply(seq_along(county_codes), \(i) {
     aqs_dailysummary_by_county(
       parameter  = params[[param]],
       bdate      = start_date,
-      # bdate      = as_date("2023/01/01"), # test with smaller date range
       edate      = end_date,
-      # stateFIPS  = state_code,
+      # test different end dates for Buffalo
+      # edate      = as_date(ifelse(
+      #   param == "PM2.5", "2001-01-01", "2015-01-01"
+      # )), 
       stateFIPS  = state_codes[[i]],
       countycode = county_codes[[i]]
     )
-      # dplyr::select(
-      #   variable = parameter, 
-      #   date     = date_local, 
-      #   unit     = units_of_measure, 
-      #   mean     = arithmetic_mean, 
-      #   state, county, city, 
-      #   latitude, longitude # , datum # can ignore datum, negligible diff
-      # ) %>% 
-      # mutate(date = as_date(date))
   })
-}, mc.cores = ncores)
-# })
-# saveRDS(data, file = "data/epa_air_pollution_data.RDS")
+# }, mc.cores = ncores)
+})
+
+saveRDS(data, file = "data/epa_air_pollution_data.RDS")
+# saveRDS(data, file = "data/epa_air_pollution_data_buffalo.RDS")
+# data <- readRDS("data/epa_air_pollution_data.RDS")
 
 # remove errors
 # TODO: Double check this doesn't accidentally remove lots of data?!
@@ -144,7 +171,20 @@ data_join <- bind_rows(data_clean) %>%
     )
   )
 
-data_join3 <- rbind(data_join, data_join1)
-setdiff(cities, data_join3$city)
+# data_join3 <- rbind(data_join, data_join1)
+# setdiff(cities, data_join3$city)
 
 readr::write_csv(data_join, "data/epa_air_pollution_data.csv")
+# readr::write_csv(data_join, "data/epa_air_pollution_data_buffalo.csv")
+
+# test plot
+data_join %>%
+  filter(city == "Buffalo") %>%
+  # ggplot(aes(x = date, y = mean, colour = variable)) +
+  # ggplot(aes(x = date, y = mean, colour = factor(site))) +
+  ggplot(aes(x = date, y = mean, colour = variable)) + 
+  geom_line(alpha = 0.5) +
+  # guides(colour = "none") + 
+  # facet_wrap(~variable, scales = "free_y") +
+  theme +
+  labs(x = "Date", y = "Mean", colour = "")

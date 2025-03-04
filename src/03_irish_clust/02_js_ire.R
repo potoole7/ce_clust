e505a36046370621b5ff85e7d5b0ce0cd6fa4227
paddy7wb <paddy7wb@gmail.com>
2025-03-04 08:55:00 +0000

    Choose k via AIC and TWGSS

--- src/03_irish_clust/02_js_ire.R
+++ src/03_irish_clust/02_js_ire.R
@@ -1,10 +1,5 @@
 #### Conditional Extremes clustering for Irish data ####
 
-# Note: Doesn't work for fixed b when keeping b values, so remove
-
-# TODO: Investigate differences in length for reglines
-# TODO: Get data for 6 counties
-
 #### Libs ####
 
 library(sf)
@@ -13,28 +8,18 @@ devtools::load_all("../evc")
 library(dplyr, quietly = TRUE)
 library(tidyr)
 library(ggplot2)
-library(lubridate)
-# library(texmex)
-devtools::load_all("texmex") # forked texmex which allows for fixed b vals
-library(gridExtra)
-library(patchwork)
-library(evgam)
-library(geosphere)
+# # devtools::load_all("texmex") # forked texmex which allows for fixed b vals
 library(circular)
-
-# for clustering
-library(cluster)
-library(proxy)
-
-# for divergence/distance measures and test statistics
-library(philentropy)
-library(transport)
-library(twosamples)
 
 source("src/functions.R")
 
 sf::sf_use_s2(FALSE)
 
+#### Metadata ####
+
+max_k <- 10 # maximum number of clusters to look at 
+# dqu <- 0.7
+dqu <- 0.9
 
 #### Load Data ####
 
@@ -59,11 +44,7 @@ ab_df <- readr::read_csv("data/ab_vals.csv") %>%
 # ab_df <- readr::read_csv("data/ab_vals_fixed_b.csv")
 
 # first check names for dependence are the same as in ab_df
-unique(ab_df$name) == names(dependence) 
-# extract point location of each station for plotting on map
-pts <- ab_df %>% 
-  distinct(name, lon, lat) %>% 
-  st_to_sf()
+all(unique(ab_df$name) == names(dependence))
 
 # remove locations with NAs for a
 na_locs <- ab_df %>% 
@@ -74,12 +55,17 @@ na_locs <- ab_df %>%
 if (length(na_locs) > 0) {
   ab_df <- filter(ab_df, !name %in% na_locs)
   dependence <- dependence[
-    !grepl(na_locs, names(dependence), fixed = TRUE)
+    !names(dependence) %in% na_locs
   ]
 }
 
-# load adjacency matrix
-adj_mat <- readRDS("data/ire_adj_mat.RDS")
+# extract point location of each station for plotting on map
+pts <- ab_df %>% 
+  distinct(name, lon, lat) %>% 
+  st_to_sf()
+
+# calculate adjacency matrix from Voronoi cells
+adj_mat <- calc_adj_mat(pts, plot = TRUE)
 
 # remove locations with NAs from data
 data <- data %>% 
@@ -92,106 +78,211 @@ ab_df_wide <- ab_df %>%
   pivot_wider(names_from = col, values_from = value)
 
 
-#### Clustering ####
+#### Choose k ####
 
 # scree plot (and distance matrix)
 set.seed(123)
-dist_mat <- js_clust(dependence, scree_k = 1:10)$dist_mat 
-# no clear elbow unfortunately, perhaps could say at k = 4?
+clust_obj <- js_clust(dependence, scree_k = 1:max_k)
+dist_mat <- clust_obj$dist_mat
+twgss <- clust_obj$total_within_ss
+# no clear elbow unfortunately
 
-# look at boxplot of silhoutte widths for different values of k
-sil_boxplot(dist_mat, k = 2:6) # looks like k = 2 is best by some distance
+# AIC
+# TODO Put in package
+# calculate AIC for a given CE fit
+calc_inf <- \(ll, n_par, n = NULL, type = "AIC") {
+  return(switch(
+    type, 
+    "AIC"  = (-2 * ll) + (2 * n_par)
+  ))
+}
 
-# Silhouette plot for different values of k (definitely best for k = 2)
-plot(silhouette(pam(dist_mat, k = 2)))
-plot(silhouette(pam(dist_mat, k = 3)))
-plot(silhouette(pam(dist_mat, k = 4)))
+# Function to calculate AIC from CE model output
+# TODO Put in package
+ce_aic <- \(dependence) {
+  # pull LLs for each model
+  lls <- vapply(dependence, \(x) {
+    vapply(x, `[`, "ll", , FUN.VALUE = numeric(1))
+  }, numeric(length(dependence[[1]])))
+  # number of parameters = 4 * number of clusters * number of variables
+  n_par <- 4 * length(dependence) * length(dependence[[1]])
+  # n par for different AICs per model
+  # combined_ll
+  ll <- sum(lls)
+  # return AIC
+  return(calc_inf(ll, n_par))
+}
 
-# plot PAM clustering solution (for k = 2 and k = 3)
-# TODO: Fix showing cluster centroid
-pam_clust2 <- js_clust(dependence, k = 2, dist_mat = dist_mat)
-plt_clust_map(pts, areas, pam_clust2)
+# cluster for different values of k, refit CE model and calculate AIC
+# TODO specific to this analysis, move to top of script
+calc_aic <- \(dist_mat, k_vals) {
+  aic <- vapply(k_vals, \(k) {
+    # cluster
+    pam_fit <- js_clust(
+      dependence, dist_mat = dist_mat, k = k # , return_dist = TRUE
+    )
+    
+    # pull medoids and their location information
+    medoids_df <- data %>% 
+      filter(name %in% pam_fit$medoids) %>% 
+      distinct(across(name:lat))
+    
+    # add data to new clusters
+    data_clust <- data %>% 
+      left_join(
+        # match locations to their cluster centroids
+        data.frame(
+          "name"   = names(pam_fit$clustering), 
+          "medoid" = pam_fit$medoids[pam_fit$clustering])
+      ) %>% 
+      # replace location information with medoid information
+      select(-names(medoids_df)) %>% 
+      left_join(medoids_df, by = c("medoid" = "name")) %>% 
+      relocate(medoid:lat, .after = date) %>% 
+      rename(name = medoid) %>% 
+      arrange(name, desc(date))
+    
+    # evgam formula will change depending on number of clusters
+    # default
+    f_marg_prob <- list("response ~ name", "~ name")
+    f <- list(excess ~ name, ~1)
+    if (k > 3) {
+        # f = list("response ~ s(lon, lat, k = 50)", "~ s(lon, lat, k = 40)")
+      f_marg_prob <- list(
+        # "response ~ s(lon, lat, k = 50)", 
+        paste0("response ~ s(lon, lat, k = ", k, ")"), # 50)", 
+        # "~ s(lon, lat, k = 40)"
+        paste0("~ s(lon, lat, k = ", k, ")")
+      )
+      f <-  list(
+        # excess ~ s(lon, lat, k = 40), 
+        paste0("excess ~ s(lon, lat, k = ", k, ")"),
+        # ~s(lon, lat, k = 40)
+        paste0("~s(lon, lat, k = ", k, ")")
+      )
+    }
+    
+    # refit CE model
+    ce_fit <- fit_ce(
+      data = data_clust,
+      vars = c("rain", "wind_speed"), 
+      # arguments to `quantile_thresh`
+      marg_prob = list(
+        # f = list("response ~ s(lon, lat, k = 50)", "~ s(lon, lat, k = 40)")
+        f = f_marg_prob
+      ), 
+      cond_prob = dqu, # dqu
+      # formula for evgam fit to excesses over threshold
+      # f = list(excess ~ s(lon, lat, k = 40), ~s(lon, lat, k = 40)), 
+      f = f,
+      fit_no_keef = FALSE, # don't fit without Keef constraints
+      ncores = max(1, parallel::detectCores() - 2),
+      output_all = TRUE
+    )
+  
+    # calculate AIC
+    ce_aic(ce_fit$dependence)
+  }, numeric(1))
+  
+  return(aic)
+}
+
+aic <- calc_aic(dist_mat, 2:max_k)
+
+# plot aic and twgss together nicely
+elb_plt <-\(twgss, aic) {
+  data.frame("elb" = twgss, "aic" = c(NA, aic), "k" = 1:max_k) %>% 
+    pivot_longer(cols = c("elb", "aic")) %>%
+    mutate(
+      value = ifelse(name == "aic", -value, value),
+      name = ifelse(name == "elb", "TWGSS", "-AIC")
+    ) %>% 
+    ggplot(aes(x = k, y = value, colour = name)) + #, group = sim_n)) + 
+    # geom_line(alpha = 0.2) + 
+    geom_line() + 
+    # geom_vline(xintercept = k_true, linetype = "dashed") +
+    facet_wrap(~name, scales = "free_y") + 
+    evc::evc_theme() + 
+    labs(
+      title = "Elbow Plots for AIC, TWGSS",
+      x     = "k", 
+      y     = ""
+    ) + 
+    scale_x_continuous(breaks = 1:max_k, limits = c(1, max_k)) + 
+    guides(colour = "none") + 
+    ggsci::scale_colour_nejm()
+}
+
+elb_plt(twgss, aic)
+# AIC seems to suggest k = 6, while TWGSS has no clear elbow 
+
+# Cluster for reasonable k, save results for later refitting of CE
+pam_clust <- lapply(2:max_k, \(k) { 
+  js_clust(dependence, k = k, dist_mat = dist_mat)
+})
+saveRDS(pam_clust, "data/pam_clust.RDS")
+
+# save map plots to see which seems most sensible
+pam_map_p_lst <- lapply(seq_along(pam_clust), \(k) {
+  plt_clust_map(pts, areas, pam_clust[[k]]) + 
+    ggtitle(paste0("k" = k + 1))
+})
+
+pdf("plots/pam_map_p_lst.pdf")
+pam_map_p_lst
+dev.off()
+
+# Conclusion: Unsure! k = 3 or 6 probably best, but no clear elbow for TWGSS
+
+#### Clustering ####
+
+# Cluster for "correct" k
 pam_clust3 <- js_clust(dependence, k = 3, dist_mat = dist_mat)
-plt_clust_map(pts, areas, pam_clust3)
+pam_clust6 <- js_clust(dependence, k = 6, dist_mat = dist_mat)
 
 # save clustering
-saveRDS(pam_clust2, "data/pam_clust2.RDS")
+saveRDS(pam_clust3, "data/pam_clust3.RDS")
+saveRDS(pam_clust6, "data/pam_clust6.RDS")
 
-# Plot silhouette coefficients on map
-plt_sil_map(
-  pts, 
-  areas, 
-  sil_obj = data.frame(silhouette(pam_clust2)) %>% 
-    arrange(rownames(.)),
-  medoids = rownames(pam_clust2$medoids)
-)
-
-plt_sil_map(
-  pts, 
-  areas, 
-  sil_obj = data.frame(silhouette(pam_clust3)) %>% 
-    arrange(rownames(.)),
-  medoids = rownames(pam_clust3$medoids)
-)
-
-# plot vs covariates (alt, dist2coast, wind_dir)
-covar_plt(pts, pam_clust2, data, areas)
+# plot vs covariates
 covar_plt(pts, pam_clust3, data, areas)
-
+covar_plt(pts, pam_clust6, data, areas)
 
 #### Adjacency stipulation clustering ####
 
 # repeat for adjacent sites only
 dist_mat_adj <- as.matrix(dist_mat)
 dist_mat_adj[adj_mat == 0] <- 1e9
+dist_mat_adj <- as.dist(dist_mat_adj)
 
-sil_adj <- sil_boxplot(dist_mat_adj, k = 2:10)
-sil_adj$plot + 
-  scale_y_continuous(limits = c(0, 0.25)) 
-sil_adj$sil %>% 
-  group_by(k) %>% 
-  summarise(sil_width = mean(sil_width)) %>% 
-  arrange(desc(sil_width)) %>% 
-  slice(1) %>% 
-  pull(k)
-# suggests k = 6! Valid to use for adjacent distance matrix?
-
-pam_clust_adj2 <- js_clust(dependence, k = 2, dist_mat = dist_mat_adj)
-plt_clust_map(pts, areas, pam_clust_adj2)
-pam_clust_adj3 <- js_clust(dependence, k = 3, dist_mat = dist_mat_adj)
-plt_clust_map(pts, areas, pam_clust_adj3)
-pam_clust_adj6 <- js_clust(dependence, k = 6, dist_mat = dist_mat_adj)
-plt_clust_map(pts, areas, pam_clust_adj6)
-
-plot(silhouette(pam_clust_adj2))
-plot(silhouette(pam_clust_adj3))
-plot(silhouette(pam_clust_adj6))
-
-plt_sil_map(
-  pts, 
-  areas, 
-  data.frame(silhouette(pam_clust_adj2)) %>% 
-    arrange(rownames(.)),
-  medoids = rownames(pam_clust_adj2$medoids)
+# calculate TWGSS and AIC
+clust_obj_adj <- js_clust(
+  dependence, dist_mat = dist_mat_adj, scree_k = 1:max_k
 )
+twgss_adj <- clust_obj_adj$total_within_ss
+aic_adj <- calc_aic(dist_mat_adj, 2:max_k)
 
-plt_sil_map(
-  pts, 
-  areas, 
-  data.frame(silhouette(pam_clust_adj3)) %>% 
-    arrange(rownames(.)),
-  medoids = rownames(pam_clust_adj3$medoids)
-)
+elb_plt(twgss_adj, aic_adj) # perhaps k = 8?
 
-plt_sil_map(
-  pts, 
-  areas, 
-  data.frame(silhouette(pam_clust_adj6)) %>% 
-    arrange(rownames(.)),
-  medoids = rownames(pam_clust_adj6$medoids)
-)
+# cluster and save data and plots
+pam_clust_adj <- lapply(2:max_k, \(k) { 
+  js_clust(dependence, k = k, dist_mat = dist_mat_adj)
+})
+saveRDS(pam_clust_adj, "data/pam_clust_adj.RDS")
+
+pam_map_p_lst_adj <- lapply(seq_along(pam_clust_adj), \(k) {
+  plt_clust_map(pts, areas, pam_clust_adj[[k]]) + 
+    ggtitle(paste0("k" = k + 1))
+})
+
+pdf("plots/pam_map_p_lst_adj.pdf")
+pam_map_p_lst_adj
+dev.off()
+
+# Plot for optimal k value
+pam_clust_adj8 <- js_clust(dependence, k = 8, dist_mat = dist_mat_adj)
+plt_clust_map(pts, areas, pam_clust_adj8)
 
 # plot vs covariates (alt, dist2coast, wind_dir)
-covar_plt(pts, pam_clust_adj2, data, areas)
-covar_plt(pts, pam_clust_adj3, data, areas)
-covar_plt(pts, pam_clust_adj6, data, areas)
+covar_plt(pts, pam_clust_adj8, data, areas)
+

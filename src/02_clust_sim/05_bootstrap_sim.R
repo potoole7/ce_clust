e6812ee535213c3f22dfd65761ab095f3e8eae3c
paddy7wb <paddy7wb@gmail.com>
2025-03-14 11:13:07 +0000

    Add more descriptive comments, plot uncertainty

--- src/02_clust_sim/05_bootstrap_sim.R
+++ src/02_clust_sim/05_bootstrap_sim.R
@@ -1,6 +1,7 @@
-#### See how uncertainty changes with bootstrapping ####
+#### Bootstrap uncertainty in CE estimates pre- and post-clustering ####
 
-# TODO: Need to use `sim` = "balanced"
+# Want to see if our clustering improves the conditional extremes parameter
+# uncertainty by bootstrapping the estimates from our simulation example
 
 #### libs ####
 
@@ -27,14 +28,14 @@ mix_p <- c("gauss_cop" = 0.5, "t_cop" = 0.5) # mixture percentages
 # GPD parameters
 scale_gpd <- 1
 shape_gpd <- -0.05
-# marg_prob <- 0.9
-marg_prob <- 0.95
-# cond_prob <- 0.9
-cond_prob <- 0.95
-conf_level <- 0.95 # confidence level for CIs in plot
+# Marginal and conditional thresholds before and after clustering
+marg_prob <- 0.9
+cond_prob <- 0.9
+marg_prob_clust <- 0.9
+cond_prob_clust <- 0.9
 
 # Number of cores to use for parallel computation
-n_cores <- detectCores() - 1 
+n_cores <- detectCores() - 1
 
 #### Test ####
 
@@ -51,61 +52,66 @@ data_mix <- sim_cop_dat(
 )$data_mix
 
 # fit CE
+set.seed(seed_number)
 ce_fit <- fit_ce(
-  data_mix, 
+  data_mix,
   marg_prob   = marg_prob,
   cond_prob   = cond_prob,
-  fit_no_keef = TRUE, 
+  fit_no_keef = TRUE,
   output_all  = TRUE
 )
 
 # run bootstrap
+set.seed(seed_number)
 bootstrap_res <- boot_ce(
-  ce_fit, 
-  R = 100, 
-  trace = 10, 
+  ce_fit,
+  R = 100,
+  trace = 10,
   ncores = ncores
 )
 
 # plot margins for both theoretical clusters
 plot_marg <- \(x) {
-  x |> 
+  x |>
     ggplot(aes(x = value, fill = parameter)) +
-    # ggplot(aes(x = value, fill = factor(cluster))) +
     # also add histograms
     geom_histogram(aes(y = ..density..), bins = 30, alpha = 0.7) +
     geom_density(alpha = 0.5) +
-    # facet_wrap(~ parameter + vars, scales = "free", nrow = 2) +
     facet_wrap(~ cluster + parameter + vars, scales = "free", nrow = 2) +
-    # facet_grid(vars ~ parameter, scales = "free") +
-    evc::evc_theme() + 
+    evc::evc_theme() +
+    guides(fill = "none") +
     theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
 }
-bootstrap_res$marginal |> 
+bootstrap_res$marginal |>
   mutate(
     cluster = ifelse(name %in% paste0("location_", 1:6), "cor_t = 0.1", "cor_t = 0.9")
-  ) |> 
+  ) |>
   plot_marg()
-  
+
 # do the same with dependence
-# TODO Very interesting that some results for no corr give 1 and -1! Say to C
+# TODO interesting that some results for no corr give 1 and -1!
 plot_dep <- \(x) {
-  x |> 
+  x |>
     ggplot(aes(x = value, fill = parameter)) +
     geom_histogram(aes(y = ..density..), bins = 30, alpha = 0.7) +
     geom_density(alpha = 0.5) +
     facet_wrap(~ cluster + parameter + vars, scales = "free", nrow = 2) +
     scale_x_continuous(limits = c(-1, 1)) +
-    evc::evc_theme() + 
+    labs(x = "", y = "") +
+    evc::evc_theme() +
+    guides(fill = "none") +
     theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
 }
-bootstrap_res$dependence |> 
+
+plt_dep_data <- bootstrap_res$dependence |>
   mutate(
-    cluster = ifelse(name %in% paste0("location_", 1:6), "cor_t = 0.1", "cor_t = 0.9"), 
+    cluster = ifelse(name %in% paste0("location_", 1:6), "cor_t = 0.1", "cor_t = 0.9"),
     vars = paste0(vars, " | ", cond_var)
-  ) |> 
-  plot_dep()
-  
+  )
+(p_dep <- plot_dep(plt_dep_data) +
+  ggtitle(paste0("CE estimates, marg_prob = ", marg_prob, ", cond_prob = ", cond_prob)))
+# ggsave(paste0("plots/ce_estimates_marg_", marg_prob, "_cond_", cond_prob, ".png"), p_dep)
+
 
 #### Bootstrap on clustered observations ####
 
@@ -120,54 +126,101 @@ data_mix_clust <- lapply(seq_len(k), \(x) {
   do.call(rbind, data_mix[clust$pam$clustering == x])
 })
 
-# TODO repeat bootstrapping and parameter extraction process
+# repeat bootstrapping and parameter extraction process
+set.seed(seed_number)
 evc_fit_clust <- fit_ce(
-  data_mix_clust, 
-  marg_prob   = marg_prob,
-  cond_prob   = cond_prob,
-  fit_no_keef = TRUE, 
+  data_mix_clust,
+  marg_prob   = marg_prob_clust,
+  cond_prob   = cond_prob_clust,
+  fit_no_keef = TRUE,
   output_all  = TRUE
 )
+set.seed(seed_number)
 bootstrap_res_clust <- boot_ce(
-  evc_fit_clust, 
-  R = 100, 
-  trace = 10, 
+  evc_fit_clust,
+  R = 100,
+  trace = 10,
   ncores = ncores
 )
 
-bootstrap_res_clust$marginal |> 
+bootstrap_res_clust$marginal |>
   mutate(
     cluster = ifelse(name == "location_1", "clust_1", "clust_2")
-  ) |> 
+  ) |>
   plot_marg()
 
 # dramatically better! But median of densities seems pretty similar?!
 plt_dep_data_clust <- bootstrap_res_clust$dependence |>
   mutate(
-    cluster = ifelse(name == "location_1", "clust_1", "clust_2"), 
+    cluster = ifelse(name == "location_1", "clust_1", "clust_2"),
     vars = paste0(vars, " | ", cond_var)
   )
-plot_dep(plt_dep_data_clust)
+(p_dep_clust <- plot_dep(plt_dep_data_clust) +
+  ggtitle(paste0("CE estimates, marg_clust = ", marg_prob_clust, ", cond_clust = ", cond_prob_clust)))
+
+pdf(paste0(
+  "plots/sim_ce_boot_marg_", marg_prob,
+  "_cond_", cond_prob,
+  "_marg_clust", marg_prob_clust,
+  "_cond_clust_", cond_prob_clust,
+  ".pdf"
+), width = 8, height = 5)
+p_dep
+p_dep_clust
+dev.off()
+
 
 # yep, alpha values very similar!!
 # Better when using cond_prob = 0.95 rather than 0.9
-plt_dep_data_clust |> 
-  group_by(cluster, vars, parameter) |> 
-  summarise(value = median(value, na.rm = TRUE), .groups = "drop") |> 
-  pivot_wider(names_from = cluster, values_from = value) |> 
+plt_dep_data_clust |>
+  group_by(cluster, vars, parameter) |>
+  summarise(value = median(value, na.rm = TRUE), .groups = "drop") |>
+  pivot_wider(names_from = cluster, values_from = value) |>
   arrange(parameter, vars)
 
-par(mfrow = c(1, 2))
-plot(evc_fit_clust$transformed$location_1, main = "cor_t = 0.1")
-plot(evc_fit_clust$transformed$location_2, main = "cor_t = 0.9")
-par(mfrow = c(1, 1))
+# par(mfrow = c(1, 2))
+# plot(evc_fit_clust$transformed$location_1, main = "cor_t = 0.1")
+# plot(evc_fit_clust$transformed$location_2, main = "cor_t = 0.9")
+# par(mfrow = c(1, 1))
 
-# TODO plot dependence parameter confidence intervals for both
-conf_level <- 0.95
-z <- qnorm((1 + conf_level) / 2)
+# plot dependence parameter confidence intervals for both
+# function to calculate confidence intervals
+boot_ce_dep_conf <- \(dep_boot_df, alpha = 0.05) {
+  dep_boot_df |>
+    relocate(value, .after = everything()) |>
+    group_by(across(1:last_col(1))) |>
+    summarise(
+      mean = mean(value, na.rm = TRUE),
+      median = median(value, na.rm = TRUE),
+      lower = quantile(value, probs = alpha / 2, na.rm = TRUE),
+      upper = quantile(value, probs = 1 - alpha / 2, na.rm = TRUE),
+      .groups = "drop"
+    )
+}
+
+# plot
+plt_ce_dep_conf <- \(x) {
+  x |>
+    ggplot(aes(x = name, y = mean, ymin = lower, ymax = upper, colour = parameter)) +
+    geom_pointrange() +
+    # scale_y_continuous(limits = c(-1, 1)) +
+    # facet_wrap(~ + vars, scales = "free", nrow = 2) +
+    facet_wrap(~ parameter + vars) +
+    evc::evc_theme() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+}
+
+# calculate confidence intervals
+plt_dep_data |>
+  mutate(name = cluster) |>
+  boot_ce_dep_conf() |>
+  plt_ce_dep_conf()
+
+boot_ce_dep_conf(plt_dep_data_clust) |>
+  plt_ce_dep_conf()
 
 
 #### Grid search ####
 
-# TODO Do for a number of simulations!! 
+# TODO Do for a number of simulations!!
 # Needed?? Maybe just doing this once is sufficient? Show Christian anyway!

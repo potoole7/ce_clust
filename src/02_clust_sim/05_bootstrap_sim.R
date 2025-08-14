46ca4ee2e2255eaff0c610a8e5b631c4e68c7073
potoole7 <paddy7wb@gmail.com>
2025-08-14 15:13:38 +0100

    Update simulation scripts to use 99th empirical quantile of Laplace distribution for MC divergence calculations

--- src/02_clust_sim/05_bootstrap_sim.R
+++ src/02_clust_sim/05_bootstrap_sim.R
@@ -3,6 +3,9 @@
 # Want to see if our clustering improves the conditional extremes parameter
 # uncertainty by bootstrapping the estimates from our simulation example
 
+# NOTE Plot objects for paper are saved below if you just want to manually
+# save them to avoid clipping LaTeX
+
 # What will plot be in paper???
 
 #### libs ####
@@ -14,10 +17,11 @@ library(parallel)
 library(ggplot2)
 library(tidyr)
 
-# source simulation functions
+# source simulation and bootstrapping functions
 source("src/functions.R")
 
 ncores <- detectCores() - 1
+
 
 #### Metadata ####
 
@@ -43,6 +47,11 @@ cond_prob_clust <- 0.9
 marg_prob <- 0.98 # marginal quantile to calculate expectation in bootstrap
 # R <- 100
 R <- 500 # number of bootstrap samples
+
+# parameters for MC estimation of JSGa
+n_mc <- 500 # number of MC samples
+mc_method <- "laplace_trunc2" # truncate Laplace using empirical dist quant
+laplace_cap <- 0.99 # Take 99th quantile
 
 # Number of cores to use for parallel computation
 n_cores <- detectCores() - 1
@@ -72,8 +81,10 @@ ce <- fit_ce(
   output_all = TRUE
 )
 
+# plot original vs transformed data
 par(mfrow = c(1, 2))
-with(ce$original[[4]], plot(rain, wind_speed, xlab = "", ylab = ""))
+# with(ce$original[[4]], plot(rain, wind_speed, xlab = "", ylab = ""))
+with(ce$original[[4]], plot(var1, var2, xlab = "", ylab = ""))
 plot(ce$transformed[[1]], xlab = "", ylab = "")
 par(mfrow = c(1, 1))
 
@@ -82,8 +93,10 @@ plt_orig <- ce$original[[4]] |>
   mutate(
     ind = "Original",
     col_var = case_when(
-      wind_speed == max(wind_speed) ~ 1, # case with weak dependence
-      wind_speed > 5 & rain > 6 ~ 2, # case with strong dependence
+      # wind_speed == max(wind_speed) ~ 1, # case with weak dependence
+      var2 == max(var2) ~ 1, # case with weak dependence
+      # wind_speed > 5 & rain > 6 ~ 2, # case with strong dependence
+      var2 > 5 & var1 > 6 ~ 2, # case with strong dependence
       # case with strong negative dependence
       TRUE ~ 4
     ),
@@ -94,24 +107,29 @@ plt_orig <- ce$original[[4]] |>
     3,
     col_var
   )) |>
-  pivot_longer(rain:wind_speed)
+  # pivot_longer(rain:wind_speed)
+  pivot_longer(var1:var2)
 
 plt_df <- bind_rows(
   plt_orig,
   data.frame(
-    "rain"       = ce$transformed[[4]][, 1],
-    "wind_speed" = ce$transformed[[4]][, 2],
-    "ind"        = "Laplace",
-    "row_n"      = seq_len(nrow(ce$transformed[[4]]))
+    # "rain"       = ce$transformed[[4]][, 1],
+    "var1" = ce$transformed[[4]][, 1],
+    # "wind_speed" = ce$transformed[[4]][, 2],
+    "var2" = ce$transformed[[4]][, 2],
+    "ind" = "Laplace",
+    "row_n" = seq_len(nrow(ce$transformed[[4]]))
   ) |>
-    pivot_longer(rain:wind_speed) |>
+    # pivot_longer(rain:wind_speed) |>
+    pivot_longer(var1:var2) |>
     mutate(col_var = plt_orig$col_var)
 ) |>
   pivot_wider(names_from = name, values_from = value) |>
   mutate(ind = factor(ind, levels = c("Original", "Laplace")))
 
 p <- plt_df |>
-  ggplot(aes(x = rain, y = wind_speed)) +
+  # ggplot(aes(x = rain, y = wind_speed)) +
+  ggplot(aes(x = var1, y = var2)) +
   # geom_point(aes(colour = factor(col_var), size = factor(col_var)), alpha = 0.7) +
   geom_point() +
   geom_point(
@@ -126,7 +144,8 @@ p <- plt_df |>
   guides(colour = "none") +
   NULL
 
-ggsave("latex/plots/transform.png", p, width = 8, height = 6)
+# TODO Save again if fixable?
+# ggsave("latex/plots/transform.png", p, width = 8, height = 6)
 
 # Function to transform data, fit CE and perform bootstrapping
 fit_boot <- \(data_mix, ncores = 1, ret_ce = FALSE) {
@@ -164,14 +183,21 @@ fit_boot <- \(data_mix, ncores = 1, ret_ce = FALSE) {
     ncores = ncores
   )
   if (ret_ce) {
-    return(list(bootstrap_res, ce_fit))
+    # return(list(bootstrap_res, ce_fit))
+    # return(list(bootstrap_res, ce_fit, data_mix_trans))
+    return(list(
+      "bootstrap_res" = bootstrap_res,
+      "ce_fit" = ce_fit,
+      "data_mix_trans" = data_mix_trans
+    ))
   }
   return(bootstrap_res)
 }
 
-bootstrap_res <- fit_boot(data_mix, ncores = ncores, ret_ce = TRUE)
-ce_fit <- bootstrap_res[[2]]
-bootstrap_res <- bootstrap_res[[1]]
+bootstrap_run <- fit_boot(data_mix, ncores = ncores, ret_ce = TRUE)
+ce_fit <- bootstrap_run$ce_fit
+data_mix_trans <- bootstrap_run$data_mix_trans
+bootstrap_res <- bootstrap_run$bootstrap_res
 
 # extract the bootstrapped data for each cluster
 boot_est <- purrr::transpose(lapply(bootstrap_res, \(x) { # loop through bootstrap samples
@@ -186,7 +212,6 @@ boot_est_df <- bind_cols(lapply(boot_est, unlist)) |>
   pivot_longer(everything(), names_to = "cluster") |>
   mutate(cluster = as.numeric(cluster)) |>
   mutate(cluster = ifelse(cluster == 1, cor_t_ex[1], cor_t_ex[2]))
-
 
 # TODO Return to add post-clustering bootstrap results
 plot_boot <- \(boot_est_df) {
@@ -215,7 +240,13 @@ plot_boot(boot_est_df)
 
 # cluster for k = 2
 k <- n_clust
-clust <- js_clust(ce_fit, k = k, cluster_mem = cluster_mem)
+clust <- js_clust(
+  ce_fit,
+  trans = data_mix_trans,
+  k = k, cluster_mem = cluster_mem,
+  n = n_mc, mc_method = mc_method, laplace_cap = laplace_cap,
+  return_dist = TRUE
+)
 # check clustering perfect
 clust$adj_rand == 1
 
@@ -224,7 +255,7 @@ data_mix_clust <- lapply(seq_len(k), \(x) {
   do.call(rbind, data_mix[clust$pam$clustering == x])
 })
 
-# repeat bootstrapping
+# repeat bootstrapping, this time on clustered observations
 bootstrap_res_clust <- fit_boot(data_mix_clust, ncores = ncores)
 
 # extract the bootstrapped data for each cluster
@@ -253,10 +284,12 @@ p_dat <- bind_rows(
       TRUE ~ "Post-clustering"
     ), levels = c("Pre-clustering", "Post-clustering"))
   )
+# TODO Investigate why histogram for pre-clustering is higher than density?
 p <- p_dat |>
   ggplot(aes(x = value)) +
   # geom_histogram(aes(y = ..density.., fill = ind), bins = 30, alpha = 0.5) +
-  geom_density(aes(fill = ind), alpha = 0.5) +
+  # geom_histogram(aes(fill = ind), bins = 30, alpha = 0.5) +
+  geom_density(aes(y = ..density.., fill = ind), alpha = 0.5) +
   facet_wrap(
     ~cluster,
     scales = "free_y",
@@ -266,10 +299,10 @@ p <- p_dat |>
   ) +
   labs(x = "Conditional Expectation", y = "", fill = "") +
   guides(fill = guide_legend(override.aes = list(alpha = 1))) +
-  evc::evc_theme() +
-  ggsci::scale_fill_nejm()
+  evc::evc_theme()
 p
 
+saveRDS(p, "plots/sim_01e_bootstrap.rds") # save plot object
 ggsave("latex/plots/sim_01e_bootstrap.png", p, width = 8, height = 5)
 
 # also do boxplot
@@ -281,4 +314,6 @@ p_box <- p_dat |>
   ggsci::scale_fill_nejm() +
   scale_x_discrete(expand = c(0.25, 0.25))
 p_box
+
+saveRDS(p_box, "plots/sim_01e_bootstrap_box.rds")
 ggsave("latex/plots/sim_01e_bootstrap_box.png", p_box, width = 8, height = 5)
